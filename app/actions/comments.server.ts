51d6def27858cbac0528b8b84f12848d8e3c0919
Sahid Rahman <sahidrahman404@gmail.com>
2023-02-08 21:37:16 +0700

    update query

--- app/actions/comments.server.ts
+++ app/actions/comments.server.ts
@@ -17,14 +17,16 @@ async function selectAllComments() {
   return transformComments(comments);
 }
 
-type A = Pick<Comment, "path" | "parentPath" | "content"> & {
-  name: string;
-  rank: number;
-};
+type A = Pick<Comment, "path" | "parentPath" | "content">;
 async function selectAllCommentsWithName() {
-  const comments = await sql<A[]>`select content, name, path, parent_path, rank 
-from comments 
-join users on comments.users_id = users.id order by rank desc`;
+  const comments = await sql<
+    A[]
+  >`select c.id as comments_id, u.id as users_id, c.created_at, c.path, c.parent_path, u.username, c.content, count(cl.comments_id) as likes
+from comments as c
+join users as u on u.id = c.users_id
+left join comments_likes as cl on cl.comments_id = c.id
+group by c.id, u.id, u.username, c.content
+order by c.created_at desc`;
 
   // const parsed = await comment.safeParseAsync(comments[0]);
   //
@@ -34,7 +36,7 @@ join users on comments.users_id = users.id order by rank desc`;
   //   return { result: parsed, origin: comments[0] };
   // }
 
-  return transformComments<A>(comments);
+  return transformComments(comments);
 }
 
 export { selectAllComments, selectAllCommentsWithName };
